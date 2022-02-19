d05dfe4349c9d36fa5602b3ebf4949e60a95362d
vChavezB <47216966+vChavezB@users.noreply.github.com>
2022-02-19 16:35:12 +0100

    updated license header for example

--- examples/Receive/Receive.ino
+++ examples/Receive/Receive.ino
@@ -1,3 +1,17 @@
+/*
+	Copyright (C) 2022 Victor Chavez
+    This file is part of SimpleJ1939.
+    SimpleJ1939 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    SimpleJ1939 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with SimpleJ1939.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include "SimpleJ1939.hpp"
 #include <mcp_can.h>
 static unsigned constexpr MCP_CS = 5; //Chip Select Pin
